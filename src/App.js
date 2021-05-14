9f1ffc72caf1ac32c4c6f2acd8a7f924ae402964
Jeremiah Tabb <>
2021-05-14 23:05:40 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  console.log('On the date this app was created, ');
+  console.log('I finished the alternative solution to ');
+  console.log('the last required lab due this week. ');
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code>!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
